f1a48b5b8b41370e30cc00ecf5fe96256368cfd0
radcheenkoo <134805103+radcheenkoo@users.noreply.github.com>
2023-07-30 10:18:03 +0300

    Initial commit

--- src/Example5.java
+++ src/Example5.java
@@ -10,13 +10,9 @@ public class Example5 {
         Iterator<T> firstIterator = first.iterator();
         Iterator<T> secondIterator = second.iterator();
 
-        while(firstIterator.hasNext() || secondIterator.hasNext()){
-            if (firstIterator.hasNext()){
+        while(firstIterator.hasNext() && secondIterator.hasNext()){
                 builder.add(firstIterator.next());
-            }
-            if (secondIterator.hasNext()){
                 builder.add(secondIterator.next());
-            }
         }
         return builder.build();
     }
